eb6b3edb0950c773748514a0eb3a8faa5bad707c
codefooo <codefooo@foo.com>
2019-09-20 09:24:43 +0900

    .

--- dapp-sample/client/src/Main.js
+++ dapp-sample/client/src/Main.js
@@ -7,6 +7,9 @@ import Glyphicon from "react-bootstrap/lib/Glyphicon";
 import Loader from 'react-loader-spinner';
 
 
+import getWeb3 from './utils/getWeb3';
+import SimpleStorage from './contracts/SimpleStorage';
+
 import './css/bootstrap.min.css';
 import './css/style.css';
 
@@ -23,6 +26,19 @@ class Main extends Component {
         storedData: '',
         pending: false
     };
+
+    async componentDidMount() {
+      //TODO
+
+    }
+
+    handleSend = async () => {
+        //TODO
+
+
+    }
+
+
 
 
     handleChange = (e) => {
@@ -41,18 +57,49 @@ class Main extends Component {
 
             <Grid fluid={true}>
                 <Row>
-                   1
+                    <Col md={5}>
+                        <InputGroup style={{paddingBottom:'10px'}}>
+                            <InputGroup.Addon>Value</InputGroup.Addon>
+                            <FormControl type="number" placeholder="Enter number" bsSize="lg" onChange={this.handleChange} />
+                        </InputGroup>
+                    </Col>
                 </Row>
                 <Row>
                     <Col md={5} style={{textAlign: "center"}}>
-                       2
+                        <div className="button">
+                            <ButtonToolbar>
+                                <ButtonGroup justified>
+                                    <Button href="#" bsStyle="primary" bsSize="large" block onClick={this.handleSend}>
+                                        Send via Metamask
+                                    </Button>
+                                </ButtonGroup>
+                            </ButtonToolbar>
+                        </div>
                     </Col>
                 </Row>
+
                 <Row style={{marginTop:'10px'}}>
                     <Col md={5}>
-                       3
+                        <Panel bsStyle="info">
+                            <Panel.Heading>
+                                <Panel.Title>
+                                    <Glyphicon glyph="signal" /> Stored Data - Event
+                                </Panel.Title>
+                            </Panel.Heading>
+                            <Panel.Body>
+                                <div style={{display:"inline-block"}}>
+                                    <p>
+                                        {this.state.storedData}
+                                    </p>
+                                </div>
+                                <div style={{display:"inline-block", float:"right"}}>
+                                    Loading Spinner
+                                </div>
+                            </Panel.Body>
+                        </Panel>
                     </Col>
                 </Row>
+
             </Grid>
 
         )
--- dapp-sample/migrations/2_deploy_contracts.js
+++ dapp-sample/migrations/2_deploy_contracts.js
@@ -3,5 +3,5 @@ var SimpleStorage = artifacts.require("SimpleStorage");
 module.exports = function(deployer) {
 
     //TODO
-    //
+
 };
