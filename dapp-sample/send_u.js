a4b6598f1a77d30666bcdaecf5776ec89adf2335
codefooo <codefooo@foo.com>
2019-10-13 09:26:22 +0900

    .

--- dapp-sample/send_u.js
+++ dapp-sample/send_u.js
@@ -1,8 +1,12 @@
-//메타마스크 계정에 이더 보내기
+//메타마스크 계정
 module.exports = function(callback) {
+
+    const toAddr = "0xAFc4F9F3bA806dd2F8e47A524fFDa2418bBFc08a";
 
     web3.eth.getAccounts().then((accounts) => {
         web3.eth.sendTransaction(
-            {from:accounts[1], to: "0xAFc4F9F3bA806dd2F8e47A524fFDa2418bBFc08a", value:web3.utils.toWei("10", "ether")}, callback);
+            {from:accounts[1],
+                to: toAddr,
+                value:web3.utils.toWei("10", "ether")}, callback);
     });
 }
