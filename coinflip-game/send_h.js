1246cd3d4f3a70801dc9884e6353f1bd232762d1
codefooo <codefooo@foo.com>
2019-10-15 18:02:28 +0900

    .

--- coinflip-game/send_h.js
+++ coinflip-game/send_h.js
@@ -1,11 +1,11 @@
 //컨트랙트
 module.exports = function(callback) {
 
-    const toAddr = "0x570e4877495F110BAbB949FC09A162fC1A29E7F6";
+    const toAddr = "0xEf0F089f0D03fF4CC1a3188068D190109126379F";
     web3.eth.getAccounts().then((accounts) => {
         web3.eth.sendTransaction(
-            {from:accounts[1],
-               to: toAddr,
+            {from:accounts[2],
+               to:toAddr,
                value:web3.utils.toWei("10", "ether")}, callback);
     });
 }
--- coinflip-game/send_u.js
+++ coinflip-game/send_u.js
@@ -1,11 +1,11 @@
 //메타마스크 계정
 module.exports = function(callback) {
 
-    const toAddr = "0xAFc4F9F3bA806dd2F8e47A524fFDa2418bBFc08a";
+    const toAddr = "0x4b005a94bb23104d05495f15d2308bd908500e6c";
     web3.eth.getAccounts().then((accounts) => {
         web3.eth.sendTransaction(
-            {from:accounts[1],
-               to: toAddr,
+            {from:accounts[2],
+               to:toAddr,
                value:web3.utils.toWei("10", "ether")}, callback);
     });
 }
