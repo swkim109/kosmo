aa3c9ae0bc2586c322030c4b452120c268110bd7
codefooo <codefooo@foo.com>
2019-11-25 07:13:31 +0900

    .

---
+++ solidity/Reentrance.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.11;
+
+contract Reentrance {
+
+    mapping(address => uint) public balances;
+
+    function donate(address _to) public payable {
+        balances[_to] += msg.value;
+    }
+
+    function balanceOf(address _who) public view returns (uint balance) {
+        return balances[_who];
+    }
+
+    function withdraw(uint _amount) public {
+
+       if (balances[msg.sender] >= _amount) {
+
+          (bool bOk, bytes memory returnData) =
+          msg.sender.call.value(_amount)(abi.encodeWithSelector(""));
+
+          if (!bOk) {
+              revert();
+          }
+
+          balances[msg.sender] -= _amount;
+       }
+    }
+
+    function() external payable {}
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
