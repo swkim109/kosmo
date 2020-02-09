0e267810e61bd197d68dccd4291f6d3ee7f5dd65
codefooo <codefooo@foo.com>
2020-02-09 23:02:09 +0900

    .

---
+++ docker/win/geth-init.ps1
@@ -0,0 +1,6 @@
+$DATADIR = "$env:USERPROFILE\edu"
+$SRC = "$env:USERPROFILE\kosmo\docker"
+
+New-Item -Path "~" -Name "edu" -ItemType "directory" -Force
+
+geth --datadir=$DATADIR --nousb init $SRC\genesis.json
---
+++ docker/win/geth-run.ps1
@@ -0,0 +1,3 @@
+$DATADIR = "$env:USERPROFILE\edu"
+
+geth --networkid 44  --datadir $DATADIR --nousb --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ws --wsaddr 0.0.0.0 --wsorigins "*" --allow-insecure-unlock
