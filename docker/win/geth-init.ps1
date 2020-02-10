c396fc6d9c9e70e6f85ba5ed7046553da8cdb87b
codefooo <codefooo@foo.com>
2020-02-10 16:39:18 +0900

    Windows Powershell

--- docker/win/geth-init.ps1
+++ docker/win/geth-init.ps1
@@ -1,6 +1,9 @@
+# Geth initialization
+
 $DATADIR = "$env:USERPROFILE\edu"
 $SRC = "$env:USERPROFILE\kosmo\docker"
 
 New-Item -Path "~" -Name "edu" -ItemType "directory" -Force
 
 geth --datadir=$DATADIR --nousb init $SRC\genesis.json
+
--- docker/win/geth-run.ps1
+++ docker/win/geth-run.ps1
@@ -1,3 +1,6 @@
+# Geth RUN
+
 $DATADIR = "$env:USERPROFILE\edu"
 
 geth --networkid 44  --datadir $DATADIR --nousb --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ws --wsaddr 0.0.0.0 --wsorigins "*" --allow-insecure-unlock
+
