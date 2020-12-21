ecf5d737ca7dd8890423ba222bf08fa900bfce60
swkim109 <swkim109@gmail.com>
2020-12-21 10:08:10 +0900

    .

--- docker/win/geth-run.ps1
+++ docker/win/geth-run.ps1
@@ -1,6 +1,8 @@
 # Geth RUN
 
 $DATADIR = "$env:USERPROFILE\edu"
+$NODE_IP = "extip:192.168.0.10"
+$NET_MASK = "192.168.0.0/24"
 
-geth --networkid 44  --datadir $DATADIR --nousb --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ws --wsaddr 0.0.0.0 --wsorigins "*" --allow-insecure-unlock
+geth --networkid 44  --datadir $DATADIR --syncmode "full" --nousb --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ws --wsaddr 0.0.0.0 --wsorigins "*" --allow-insecure-unlock --nat $NODE_IP --netrestrict $NET_MASK
 
