c549682ffd7d7beec4a81c8d9a7e662e20ce5f7a
swkim109 <swkim109@gmail.com>
2021-01-10 13:27:57 +0900

    .

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './MyFunction';
+import App from './MyComp';
 
 import * as serviceWorker from './serviceWorker';
 
